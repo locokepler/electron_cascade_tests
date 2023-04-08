2e9b5fe2113c2d7ecd4cd9e28b1ff08dd56e832e
locokepler <locokepler@gmail.com>
2023-04-08 10:26:54 -0500

    does secondaries iteration with an external source phsp start

---
+++ externally_generated_secondary_iterate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd /Users/keplerdomurat-sousa/Desktop/electron_cascade_tests
+make secondaries
+cd /Users/keplerdomurat-sousa/Desktop/electron_cascade_tests/run_zone/
+rm ./*
+cp ../$1 ./
+mv $1 full_HGM.phsp
+
+for i in {0..28}; do
+	echo "$i iteration"
+	../secondaries full_HGM timings output backplane.phsp ../$2
+	/Applications/topas/bin/topas ../load_phasespace_HGM.topas
+done
+
+
