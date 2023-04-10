f24e094da98c7d72203eaddd2e729a348110d56f
locokepler <locokepler@gmail.com>
2023-04-09 20:52:40 -0500

    now using c version and cleaning up previous backplanes

--- externally_generated_secondary_iterate.sh
+++ externally_generated_secondary_iterate.sh
@@ -3,6 +3,7 @@ cd /Users/keplerdomurat-sousa/Desktop/electron_cascade_tests
 make secondaries
 cd /Users/keplerdomurat-sousa/Desktop/electron_cascade_tests/run_zone/
 rm ./*
+rm ../$2
 cp ../$1 ./
 mv $1 full_HGM.phsp
 
--- secondary_iterate.sh
+++ secondary_iterate.sh
@@ -2,11 +2,12 @@
 cd /Users/keplerdomurat-sousa/Desktop/electron_cascade_tests/run_zone/
 rm ./*
 
+
 /Applications/topas/bin/topas ../phasespace_HGM_run.topas
 
 for i in {0..28}; do
 	echo "$i iteration"
-	python3 ../secondary_electron_bodge.py
+	../secondaries full_HGM timings output backplane.phsp ../combined_backplane.phsp
 	/Applications/topas/bin/topas ../load_phasespace_HGM.topas
 done
 
