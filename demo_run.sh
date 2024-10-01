f502154da801ccf13fc9904edb295bee8db2d987
locokepler <locokepler@gmail.com>
2024-10-01 17:22:31 -0500

    demo run files

---
+++ demo_run.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+# This script should be able to take a brand new copy of this from GitHub and run
+# a basic electron cascade demo. It makes its own starting electron.
+
+# make an example LMCP like structure using the included python code
+python3 millichannel_slab.py > demo_LMCP.topas
+
+./externally_generated_secondary_iterate.sh demo_electron.phsp demo_backplane.phsp
+
+# python3 backplane_animator.py
--- externally_generated_secondary_iterate.sh
+++ externally_generated_secondary_iterate.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 # cd ~/electron_cascade_tests
 make secondaries
+if [ ! -d "run_zone" ]; then
+	mkdir run_zone
+fi
 cd run_zone/
-rm ./*
+pwd
+rm ../run_zone/* # just a little protection so it can't accidentally delete the parent directory's contents
+# ask me why I set it up this way lol
 rm ../$2
 cp ../$1 ./
 mv $1 full_HGM.phsp
 
-for i in {0..60}; do
+for i in {0..20}; do
 	echo "$i iteration"
 	../secondaries full_HGM timings output backplane.phsp ../$2
-	retVal=$?
-	if [$retVal -ne 0]; then
-		exit $retVal
-	fi
+	# retVal=$?
+	# if [$retVal -ne 0]; then
+	# 	exit $retVal
+	# fi
 	# rm backplane.phsp backplane.header
-	~/topas/bin/topas ../load_phasespace_HGM.topas
-	retVal=$?
-	if [$retVal -ne 0]; then
-		exit $retVal
-	fi
+	/Applications/topas/bin/topas ../load_phasespace_HGM.topas
+	# retVal=$?
+	# if [$retVal -ne 0]; then
+	# 	exit $retVal
+	# fi
 
 done
 
